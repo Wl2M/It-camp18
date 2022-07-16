df9f0b38ac3b5082adda9026fba538676710c474
LAB203\LAB203 <thanabordee@it.kmitl.ac.th>
2022-07-16 15:01:21 +0700

    init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { firestorePlugin} from 'vuefire'
 
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(firestorePlugin)
   .mount('#app')
+
+
